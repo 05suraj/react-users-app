08ee82db7dddef6522da843713d21d572a44f25f
suraj <suraj@gmail.com>
2021-08-29 17:41:50 +0530

    this comit is last

--- src/components/gitfolder/gitnew.js
+++ src/components/gitfolder/gitnew.js
@@ -2,19 +2,18 @@ import React from 'react'
 
 function gitnew() {
 
-    const add = (a,b) => {
-      return a+b;
+    const add = (a, b) => {
+        return a + b;
     }
     add(5, 6)
-    
-    let a = "suraj";
-    let h = "kumar";
+
+
 
 
 
     return (
         <div>
-            
+
         </div>
     )
 }
