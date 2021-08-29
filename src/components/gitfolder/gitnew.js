85a2d99b327214db537c71484a3f5a09f02d86a6
suraj <suraj@gmail.com>
2021-08-29 17:08:56 +0530

    this is add fun

---
+++ src/components/gitfolder/gitnew.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function gitnew() {
+
+    const add = (a,b) => {
+      return a+b;
+    }
+     add(5,6)
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default gitnew
